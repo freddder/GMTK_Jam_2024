e7d353740cca8a7b8414c9f0a9077b400ee0abbb
Tonetfal <antonio.sid99@gmail.com>
2024-08-17 16:18:59 +0200

    ground pount

--- scripts/player.gd
+++ scripts/player.gd
@@ -25,6 +25,9 @@ enum AttackType {
 @export var default_thrust_radius: float = 100.0
 @export var default_thrust_thickness: float = 35.0
 
+@export_category("Ground Pound")
+@export var default_ground_pound_radius: float = 100.0
+
 @export var default_camera_zoom: Vector2 = Vector2(2.3, 2.3)
 
 @export_category("Charge")
@@ -99,11 +102,18 @@ func draw_attack_zone_thrust_implementation() -> void:
 	colors.append(Color.RED)
 	
 	draw_polygon(packed_array, colors)
+	
+func draw_attack_zone_ground_pound_implementation() -> void:
+	var origin := Vector2.ZERO
+	var width := 2.0
+
+	draw_circle(origin, active_attack_zone_data.radius, attack_area_color, width)
 
 func _draw() -> void:
 	match charging_attack_type:
 		AttackType.Swing: draw_attack_zone_swing_implementation()
 		AttackType.Thrust: draw_attack_zone_thrust_implementation()
+		AttackType.GroundPound: draw_attack_zone_ground_pound_implementation()
 
 func draw_attack_zone_swing(attack_zone_data: AttackZoneData) -> void:
 	attack_area_color = Color.RED
@@ -118,6 +128,12 @@ func draw_attack_zone_thrust(attack_zone_data: AttackZoneData) -> void:
 
 	queue_redraw()
 
+func draw_attack_zone_ground_pound(attack_zone_data: AttackZoneData) -> void:
+	attack_area_color = Color.RED
+	active_attack_zone_data = attack_zone_data
+
+	queue_redraw()
+	
 func handle_mouse_direction() -> void:
 	var mouse_position := get_local_mouse_position()
 	mouse_direction_angle_rad = atan2(mouse_position.y, mouse_position.x) + PI / 2.0
@@ -154,11 +170,22 @@ func get_attack_zone_data_thrust() -> AttackZoneData:
 	attack_zone_data.angle = mouse_direction_angle_rad
 	print(mouse_direction_angle_rad)
 	return attack_zone_data
+	
+func get_attack_zone_data_ground_pound() -> AttackZoneData:
+	var charge_power := get_charge_power()
+	var attack_zone_data: AttackZoneData = AttackZoneData.new()
+
+	var radius_multiplier := maxf(1.0, pow(charge_power, 1.13))
+	attack_zone_data.radius = default_swing_radius * radius_multiplier
+
+	return attack_zone_data
+	
 
 func handle_attack_zone() -> void:
 	match charging_attack_type:
 		AttackType.Swing: draw_attack_zone_swing(get_attack_zone_data_swing())
 		AttackType.Thrust: draw_attack_zone_thrust(get_attack_zone_data_thrust())
+		AttackType.GroundPound: draw_attack_zone_ground_pound((get_attack_zone_data_ground_pound()))
 
 	if is_charging_attack():
 		var zoom_power := get_charge_power() - 1.0
@@ -189,13 +216,16 @@ func clear_attack_data() -> void:
 
 	# Return to original camera zoom
 	set_camera_zoom(default_camera_zoom, 0.4)
+	
+func on_attack_started_charging(attack_type: AttackType) -> void:
+	charging_attack_type = attack_type
+	attack_charge_start_time = Time.get_ticks_msec()
 
 func handle_primary_attack_input(event: InputEvent) -> void:
 	var attack_type := AttackType.Swing
 	var action_name := "primary_attack"
 	if !is_charging_attack() && event.is_action_pressed(action_name):
-		charging_attack_type = attack_type
-		attack_charge_start_time = Time.get_ticks_msec()
+		on_attack_started_charging(attack_type)
 
 	if charging_attack_type == attack_type && event.is_action_released(action_name):
 		var attack_circle_shape := CircleShape2D.new()
@@ -217,8 +247,7 @@ func handle_secondary_attack_input(event: InputEvent) -> void:
 	var attack_type := AttackType.Thrust
 	var action_name := "secondary_attack"
 	if !is_charging_attack() && event.is_action_pressed(action_name):
-		charging_attack_type = attack_type
-		attack_charge_start_time = Time.get_ticks_msec()
+		on_attack_started_charging(attack_type)
 
 	if charging_attack_type == attack_type && event.is_action_released(action_name):
 		var attack_rectangle_shape := RectangleShape2D.new()
@@ -235,12 +264,33 @@ func handle_secondary_attack_input(event: InputEvent) -> void:
 			collider.queue_free()
 
 		clear_attack_data()
+		
+func handle_heavy_attack_input(event: InputEvent) -> void:
+	var attack_type := AttackType.GroundPound
+	var action_name := "heavy_attack"
+	if !is_charging_attack() && event.is_action_pressed(action_name):
+		on_attack_started_charging(attack_type)
+
+	if charging_attack_type == attack_type && event.is_action_released(action_name):
+		var attack_circle_shape := CircleShape2D.new()
+		$AttackShapeCast2D.shape = attack_circle_shape
+		attack_circle_shape.radius = active_attack_zone_data.radius
+
+		$AttackShapeCast2D.force_shapecast_update()
+		for collider_index in $AttackShapeCast2D.get_collision_count():
+			var collider := $AttackShapeCast2D.get_collider(collider_index) as Node2D
+			print("Damage ", $AttackShapeCast2D.get_collider(collider_index))
+			collider.queue_free()
+
+		clear_attack_data()
 
 func _input(event: InputEvent) -> void:
 	if event.is_action("primary_attack"):
 		handle_primary_attack_input(event)
 	elif event.is_action("secondary_attack"):
 		handle_secondary_attack_input(event)
+	elif event.is_action("heavy_attack"):
+		handle_heavy_attack_input(event)
 
 func is_charging_attack() -> bool:
 	return charging_attack_type != AttackType.Invalid
