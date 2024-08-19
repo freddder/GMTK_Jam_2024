9474688d9b2557ddac790a1a1aab142e6eff1ec5
Lucas Magalhaes <lucasfredmagalhaes@gmail.com>
2024-08-19 00:18:56 -0400

    Added pickup icons and float animation

--- scripts/pickup.gd
+++ scripts/pickup.gd
@@ -4,10 +4,11 @@ extends Node2D
 enum Type {
 	Heal,
 	Wipe,
-	Invalid = -1,
+	Invalid, # Always leave invalid as last
 }
+var heal_icon := preload("res://content/sprites/pickups/pickup_heal.png")
+var wipe_icon := preload("res://content/sprites/pickups/pickup_wipe.png")
 
-@export var drop_chances: Gradient
 @onready var player: PlayerCharacter = get_tree().get_first_node_in_group("player")
 
 var should_fly_towards_player := false
@@ -34,9 +35,8 @@ func _on_fly_trigger_area_area_entered(area: Area2D) -> void:
 
 
 func randomize_pickup_type() -> Type:
-	var sampled := drop_chances.sample(randf())
-	for idx in drop_chances.colors.size():
-		if sampled == drop_chances.get_color(idx):
-			$ColorRect.color = sampled
-			return idx
-	return Type.Invalid
+	var type: Type = Type.values()[randi() % Type.size() - 1]
+	match type:
+		Type.Heal: $Sprite2D.set_texture(heal_icon)
+		Type.Wipe: $Sprite2D.set_texture(wipe_icon)
+	return type
