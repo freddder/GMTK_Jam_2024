d87304c561dcc92c03e942e59e9ea6704935f6cb
Lucas Magalhaes <lucasfredmagalhaes@gmail.com>
2024-08-20 11:29:12 -0400

    Victory theme implementation wip

--- scripts/main_level.gd
+++ scripts/main_level.gd
@@ -7,6 +7,7 @@ func _ready() -> void:
 	Events.on_player_death.connect(_on_player_death)
 	Events.on_enemy_spawned.connect(_on_enemy_spawned)
 	Events.on_boss_spawned.connect(_on_boss_spawned)
+	Events.on_game_victory.connect(play_victory_theme)
 	Events.done_playing_death_sfx.connect(play_death_theme)
 
 	var tween := get_tree().create_tween()
@@ -37,11 +38,12 @@ func _on_boss_spawned(boss: BossCharacter) -> void:
 func _on_boss_death(boss: BossCharacter)-> void:
 	add_score(boss.default_score)
 
-
 func add_score(_score: float) -> void:
 	score += _score
 	$GameHUD/ScoreLabel.text = "Score: " + str(score)
 
-
 func play_death_theme() -> void:
 	$DeathTheme.play()
+
+func play_victory_theme() -> void:
+	$VictoryTheme.play()
