f60257e6db9ee557ca3aa7f825e4fbd1f01368d0
Lucas Magalhaes <lucasfredmagalhaes@gmail.com>
2024-08-20 08:40:40 -0400

    Clear pickups after winning

--- scripts/game_victory_menu.gd
+++ scripts/game_victory_menu.gd
@@ -16,6 +16,7 @@ func _on_game_victory() -> void:
 func _on_restart_button_pressed() -> void:
 	get_tree().paused = false
 	get_tree().call_group("enemy", "queue_free")
+	get_tree().call_group("pickups", "queue_free")
 	get_tree().call_group("boss", "queue_free")
 	get_tree().change_scene_to_file("res://scenes/main_level.tscn")
 
@@ -23,6 +24,7 @@ func _on_restart_button_pressed() -> void:
 func _on_main_menu_button_pressed() -> void:
 	get_tree().paused = false
 	get_tree().call_group("enemy", "queue_free")
+	get_tree().call_group("pickups", "queue_free")
 	get_tree().call_group("boss", "queue_free")
 
 	get_tree().change_scene_to_file("res://scenes/main_menu.tscn")
